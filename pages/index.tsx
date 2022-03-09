7b411b462a87100647d1da507cf49ae4b78ace16
Benny Hernandez <mail@bennyhernandez.com>
2022-03-09 18:48:41 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import type { NextPage } from "next";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+import Copyright from "../src/Copyright";
+
+const Home: NextPage = () => {
+  return <Container maxWidth="lg"></Container>;
+};
+
+export default Home;
