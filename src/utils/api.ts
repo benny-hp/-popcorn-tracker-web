edc530f6d8321c0e82d22f3f4fd18b5d9e7359a7
Benny Hernandez <mail@bennyhernandez.com>
2022-03-15 16:06:10 -0400

    Feature: added get movie and update movie api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
-import { Movie } from "../interfaces";
+import { Movie, UpdateMovie } from "../interfaces";
 
 const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
 
 export function getMovies(): Promise<Movie[]> {
   return api.get<Movie[]>("/movies").then((res) => res.data);
+}
+export function getMovie(id: string): Promise<Movie> {
+  return api.get<Movie>(`/movies/${id}`).then((res) => res.data);
+}
+export function updateMovie(id: string, data: UpdateMovie): Promise<Movie> {
+  return api
+    .patch<Movie>(`/movies/${id}/watched`, data)
+    .then((res) => res.data);
 }
