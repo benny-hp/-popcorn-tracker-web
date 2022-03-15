67d0b51fded777089b7b73bc98ea247ec0bc1fb0
Benny Hernandez <mail@bennyhernandez.com>
2022-03-15 16:19:38 -0400

    Feature: started search page

---
+++ pages/search/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Nav from "../../src/components/Nav";
+import useMovies from "../../src/hooks/useMovies";
+import { Grid } from "@mui/material";
+import { Wrapper } from "../../src/components/ui";
+import MovieCard from "../../src/components/MovieCard";
+
+const Home: NextPage = () => {
+  const { getMoviesApi } = useMovies();
+  const { data } = getMoviesApi();
+
+  return (
+    <>
+      <Nav />
+      <Wrapper>
+        {/* <Grid container columnSpacing={2} rowGap={2}>
+          {data?.map((movie) => (
+            <MovieCard data={movie} key={movie.id} />
+          ))}
+        </Grid> */}
+      </Wrapper>
+    </>
+  );
+};
+
+export default Home;
