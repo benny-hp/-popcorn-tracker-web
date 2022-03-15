020fefdcb2a3134ed2741568d0969152f0e16341
Benny Hernandez <mail@bennyhernandez.com>
2022-03-15 16:01:07 -0400

    Refactor: update nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,27 +7,54 @@ import {
   Typography,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import React from "react";
+import Link from "./Link";
 
-const Nav = () => {
+interface Props {
+  returnButton?: boolean;
+}
+
+const Nav = ({ returnButton }: Props) => {
   return (
     <Box bgcolor={"primary.main"}>
       <Container maxWidth="lg" disableGutters={false}>
         <AppBar position="static" sx={{ boxShadow: "none" }}>
           <Toolbar disableGutters>
-            <Typography variant="h6" component="h2" sx={{ cursor: "pointer" }}>
+            <Typography
+              variant="h6"
+              component={Link}
+              href="/"
+              color="text.primary"
+              sx={{ textDecoration: "none" }}
+            >
               Popcorn Tracker
             </Typography>
-
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="add note"
-              sx={{ marginLeft: "auto" }}
-            >
-              <AddIcon />
-            </IconButton>
+            {returnButton ? (
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="add note"
+                LinkComponent={Link}
+                href="/"
+                sx={{ marginLeft: "auto" }}
+              >
+                <ArrowBackIcon />
+              </IconButton>
+            ) : (
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="add note"
+                sx={{ marginLeft: "auto" }}
+                LinkComponent={Link}
+                href="/search/"
+              >
+                <AddIcon />
+              </IconButton>
+            )}
           </Toolbar>
         </AppBar>
       </Container>
