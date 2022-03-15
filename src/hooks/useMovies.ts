5dd75e72e65103fbefc9623d2c9f81866e1e74b9
Benny Hernandez <mail@bennyhernandez.com>
2022-03-15 16:07:36 -0400

    Feature: create get movie and update movie hook

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,13 +1,29 @@
-import { useQuery } from "react-query";
-import { Movie } from "../interfaces";
-import { getMovies } from "../utils/api";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { Movie, UpdateMovie } from "../interfaces";
+import { getMovie, getMovies, updateMovie } from "../utils/api";
 
 const useMovies = () => {
   const getMoviesApi = () => {
     return useQuery<Movie[], Error>("movies", getMovies);
   };
+
+  const getMovieById = (id: string) => {
+    return useQuery<Movie, Error>(["movie", id], () => getMovie(id));
+  };
+
+  const updateMovieById = (id: string) => {
+    const queryClient = useQueryClient();
+    return useMutation((data: UpdateMovie) => updateMovie(id, data), {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries("movies");
+        queryClient.setQueryData(["movie", id], data);
+      },
+    });
+  };
   return {
     getMoviesApi,
+    getMovieById,
+    updateMovieById,
   };
 };
 
