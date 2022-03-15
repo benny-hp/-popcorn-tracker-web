83ac207491d6c06c9c7e7e5687504d3b13c82967
Benny Hernandez <mail@bennyhernandez.com>
2022-03-15 16:10:10 -0400

    Feature: create movie by id page

---
+++ pages/movie/[id].tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from "next";
+import Nav from "../../src/components/Nav";
+import useMovies from "../../src/hooks/useMovies";
+import { Wrapper } from "../../src/components/ui";
+import { useRouter } from "next/router";
+import MovieDetails from "../../src/components/MovieDetails";
+import { UpdateMovie } from "../../src/interfaces";
+
+const MovieDetailsPage: NextPage = () => {
+  const {
+    query: { id },
+  } = useRouter();
+  const { getMovieById, updateMovieById } = useMovies();
+  const { data } = getMovieById(id as string);
+  const { mutate: updateMovie } = updateMovieById(id as string);
+
+  const handleUpdate = (watched: UpdateMovie) => {
+    updateMovie(watched);
+  };
+
+  return (
+    <>
+      <Nav returnButton />
+      <Wrapper>
+        {data && <MovieDetails data={data} handleUpdate={handleUpdate} />}
+      </Wrapper>
+    </>
+  );
+};
+
+export default MovieDetailsPage;
